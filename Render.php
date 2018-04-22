837f2645a7b44b93ccdaec7524297dccc5b840c8
YuldashevR <38634209+YuldashevR@users.noreply.github.com>
2018-04-23 01:46:49 +0500

    Add files via upload

--- Render.php
+++ Render.php
@@ -7,7 +7,6 @@ class Render {
 		extract($data, EXTR_OVERWRITE);
 		require_once($file.".php");
 		$data = ['content' => ob_get_contents()];
-		ob_clean();
 		extract($data, EXTR_OVERWRITE);
 		ob_end_flush();
 	}
